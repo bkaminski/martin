38ecebfef09a7e370649f1556c719dfacdda8796
Ben Kaminski <ben@benkaminski.com>
2023-08-31 21:23:15 -0400

    finished out home page, added NextGen Gallery

--- parts/home-galleries.php
+++ parts/home-galleries.php
@@ -1,3 +1,24 @@
 <section class="home-galleries pt-5 pb-5">
     <h2 class="text-center text-white">Photo Galleries</h2>
+    <div class="container pb-5">
+        <div class="row text-white text-center">
+            <div class="col-lg-4">
+                <?php echo do_shortcode('[ngg src="galleries" ids="6" display="basic_imagebrowser" thumbnail_width="200"]'); ?>
+                <p class="h4 fw-bold">Baths</p>
+            </div>
+            <div class="col-lg-4">
+                <?php echo do_shortcode('[ngg src="galleries" ids="5" display="basic_imagebrowser" thumbnail_width="200"]'); ?>
+                <p class="h4 fw-bold">Renovations &amp; New Homes</p>
+            </div>
+            <div class="col-lg-4">
+                <?php echo do_shortcode('[ngg src="galleries" ids="7" display="basic_imagebrowser" thumbnail_width="200"]'); ?>
+                <p class="h4 fw-bold">Permanent Holiday Lighting</p>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-5">
+        <div class="col text-center">
+            <a href="galleries" class="btn btn-lg btn-warning text-uppercase fw-bold p-3 text-white">See All Galleries</a>
+        </div>
+    </div>
 </section>
