7009e4ce4dd715f5a5bf47083f80a5592c063d3b
Ben Kaminski <ben@benkaminski.com>
2023-09-12 15:30:04 -0400

    finished contact page

--- parts/page-contact.php
+++ parts/page-contact.php
@@ -1,8 +1,34 @@
 <section class="contact">
+    <div class="container pb-5">
+        <div class="text-center">
+            <p class="px-5">Please fill out the form below and we will contact you. With offices in Newark, to service New Castle, Kent and Cecil Co Md. & Harbeson, to service Sussex Co, Ocean City, Salisbury, and Cambridge Areas</p>
+        </div>
+    </div>
     <div class="container">
-        <!-- BT Contact Form -->
-        <script type="text/javascript" src="https://buildertrend.net/leads/contactforms/js/btClientContactForm.js"></script>
-        <iframe src="https://buildertrend.net/leads/contactforms/ContactFormFrame.aspx?builderID=77863" scrolling="no" id="btIframe" style="background: transparent; border: 0px; margin: 0px auto; width: 100%; min-height: auto; height: 1618px;" data-ruffle-polyfilled="" height="1618px"></iframe>
-        <!-- /BT Contact Form -->
+        <div class="row">
+            <div class="col-md-6">
+                <!-- BT Contact Form -->
+                <script type="text/javascript" src="https://buildertrend.net/leads/contactforms/js/btClientContactForm.js"></script>
+                <iframe src="https://buildertrend.net/leads/contactforms/ContactFormFrame.aspx?builderID=77863" scrolling="no" id="btIframe" style="background: transparent; border: 0px; margin: 0px auto; width: 100%; min-height: auto; height: 1618px;" data-ruffle-polyfilled="" height="1618px"></iframe>
+            </div>
+            <!-- /BT Contact Form -->
+            <div class="col-md-6">
+                <h3 class="text-red">Newark Office</h3>
+                <div class="ratio ratio-4x3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3071.918189934036!2d-75.76806012265352!3d39.651555671573014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c7a90b5b560521%3A0x359274f9683885b6!2sMartin%20Construction%20Services%20LLC!5e0!3m2!1sen!2sus!4v1694544713282!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="short-underline-1 text-red pt-5"></div>
+                <h3 class="text-red pt-5"><?php echo ('Harbeson Office'); ?></h3>
+                <div class="ratio ratio-4x3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3114.8545365736304!2d-75.2432149226815!3d38.675210971772174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b8c71d56f87493%3A0xcf5c936e782548bd!2sMartin%20Construction%20Services%20LLC!5e0!3m2!1sen!2sus!4v1694544568900!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="short-underline-1 text-red pt-5"></div>
+                <h3 class="text-red text-underline pt-5">Contact Information</h3>
+                <p class="text-red pb-3"><i class="far fa-clock"></i>&nbsp;&nbsp;Office Hours:<br>Weekdays: 7:30am to 4pm | Lunch: 12pm - 12:30pm<br>Weekends: Closed</p>
+                <p class="text-red pb-3"><i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;Address:<br>340 West Chestnut Hill Road<br>Newark, Delaware 19713</p>
+                <p class="text-red pb-3"><i class="fas fa-mobile-alt"></i>&nbsp;&nbsp;Phone:<br><a href="tel:1+3022000885">302.200.0885</a></p>
+                <p class="text-red pb-3"><i class="far fa-envelope"></i>&nbsp;&nbsp;Email:<br><a href="mailto:info@martincsllc.com">info@martincsllc.com</a></p>
+            </div>
+        </div>
     </div>
 </section>
