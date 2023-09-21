a72caf3970ee85445e06766a7c5ed000d39a146e
Ben Kaminski <ben@benkaminski.com>
2023-09-21 12:35:56 -0400

    added news section and posts

--- home.php
+++ home.php
@@ -1,31 +1,39 @@
 <?php get_header(); ?>
 <section class="int-page-header pb-5" style="padding-top: 15rem;">
 	<h1 class="text-center text-white">News</h1>
-		
 </section>
 <div class="container">
-	<div class="row mt-5">
-		<div class="col-md-9 mb-4">
-			<section>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="row mt-5">
+				<div class="col-md-3 mb-4">
+					<figure>
 						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-								
+							<?php the_post_thumbnail('medium', array('class' => 'img-thumbnail shadow')); ?>
+
 						</a>
-					</h2>
-					<small><em>Posted on: <?php echo get_the_date(); ?></em></small>
-					<?php the_excerpt(); ?>
-					<hr>
-					<?php endwhile; else : ?>
-						<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-					<?php endif; ?>
-			</section>
-		</div>
-	</div>
+					</figure>
+				</div>
+				<div class="col-md-9 mb-4">
+					<article>
+						<h2>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+
+							</a>
+						</h2>
+						<small><em>Posted on: <?php echo get_the_date(); ?></em></small>
+						<?php the_excerpt(); ?>
+					</article>
+				</div>
+			</div>
+			<hr>
+		<?php endwhile;
+	else : ?>
+		<p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+	<?php endif; ?>
 	<div class="col text-center">
 		<?php echo bkc_pagination(); ?>
-			
 	</div>
 </div>
 <?php get_footer(); ?>
+
--- single.php
+++ single.php
@@ -3,8 +3,29 @@
     <?php the_title('<h1 class="text-center text-white">', '</h1>'); ?>
 
 </section>
-<div class="container pt-5" style="padding-top: 15rem;">
-    <?php echo get_template_part('parts/page', 'page-body'); ?>
+<main>
+    <div class="container pt-5 pb-5">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="pb-3">
+                    <?php echo get_the_post_thumbnail($post_id, 'full'); ?>
+                </div>
 
+                <?php the_content(); ?>
+
+                <a class="btn btn-lg btn-warning text-uppercase" href="<?php echo get_home_url(); ?>/contact">Contact Us Today</a>
+
+            <?php endwhile;
+        else : ?>
+            <p><?php esc_html_e('Sorry, no posts or pages matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </div>
+</main>
+<div class="tag-container">
+    <div class="container">
+        <div class="col pb-3 pt-3">
+            <?php the_tags('<span class="badge bg-dark"><i class="fa-solid fa-tags"></i>&nbsp;Related Topics:</span>&nbsp;&nbsp;'); ?>
+
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
